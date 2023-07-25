640ed5b48c0fe15753aca791c221bb9b1b3ca08e
VicJo <VicJohnathanWilson@outlook.com>
2023-07-25 09:11:15 +0800

    Adding features to items (incomplete)

--- source/items.py
+++ source/items.py
@@ -1,4 +1,13 @@
 class Item:
     """Base class for in game items that can be pickup by the player."""
-    def __init__(self, name: str):
+
+    def __init__(self, name: str, item_description: str, heal_amount: int):
         self.name = name
+        self.item_description = item_description
+        self.heal_amount = heal_amount  # heal items have a non-zero heal_amount value
+
+    def read_description(self):
+        return self.item_description
+
+    def heal_player(self):
+        return self.heal_amount
